472c7d66d500fe673a332f9c15ba14470d983ec5
Hemanth Kumar <hemanthhm693@gmail.com>
2025-02-25 23:08:42 +0530

    595 problem no added

---
+++ 595 Big Countries.sql
@@ -0,0 +1,8 @@
+SELECT name,population,area 
+FROM World 
+WHERE area>=3000000 OR
+population>=25000000;
+
+# First Selected the column need to display 
+
+# Second Implement the condition based on the question 
