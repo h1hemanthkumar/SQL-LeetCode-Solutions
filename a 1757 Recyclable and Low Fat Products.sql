edd7b67ead3db3017b2e6ab0e4b53039e41c9ab1
Hemanth Kumar <hemanthhm693@gmail.com>
2025-02-25 23:17:36 +0530

    1757 problem no is added

---
+++ a 1757 Recyclable and Low Fat Products.sql
@@ -0,0 +1,8 @@
+SELECT product_id 
+FROM Products 
+WHERE low_fats='Y' AND 
+recyclable='Y';
+
+# select the required column in this case it is product_id
+
+# write the condition for both low fat and recyclable use and logical operator because of low fat as well as recyclable product id required
