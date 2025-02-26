efbb9c0f87f391c361b89c5e79c747e8df03153a
Hemanth Kumar <hemanthhm693@gmail.com>
2025-02-26 23:20:07 +0530

    1683 problem no added

---
+++ A 1683 Invalid Tweets.sql
@@ -0,0 +1,7 @@
+SELECT tweet_id 
+FROM Tweets 
+WHERE LENGTH(content)>15;
+
+# The first statement select the tweet_id to display the id of the person content greater than 15
+
+# The Length() function provide the number of character and it is check whether is within 15 character
