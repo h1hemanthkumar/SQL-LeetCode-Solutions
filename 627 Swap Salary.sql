dd1a73aa1723145d2d2cbbb4a368b926999a4b4a
Anupamagowda23 <anupamagowda23@gmail.com>
2025-03-03 14:03:26 +0530

    an optimized solution to 627 added

--- 627 Swap Salary.sql
+++ 627 Swap Salary.sql
@@ -5,3 +5,13 @@ SET sex = CASE
 END;
 
 # update salary with two cases on if sex='f' means female change it to 'm' means male and via versa this is an example for case statement problem 
+
+
+#OR
+
+UPDATE Salary
+SET sex = REPLACE(REPLACE(sex, 'f', 'x'), 'm', 'f'), 'x', 'm');
+
+#1.Using 'IF' (MySQL-specific): The 'IF' function checks if sex is 'f', then changes it to 'm', otherwise, it changes 'm' to 'f'.  
+
+# Using REPLACE: This replaces 'f' with a temporary placeholder ('x'), then swaps 'm' to 'f', and finally changes 'x' to 'm', effectively swapping values without conditions.
